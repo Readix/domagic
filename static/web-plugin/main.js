7761e741605089e23a77e01e8df5f3ea89b20e77
ega3002 <egor@DESKTOP-LIT7DLV.localdomain>
2020-09-11 02:44:44 +0500

    dices creating by emojis for now, fixed bug with shown roll icon on other pictures

--- static/web-plugin/main.js
+++ static/web-plugin/main.js
@@ -16,7 +16,7 @@ viewBox="0 0 55.333 55.333" style="enable-background:new 0 0 55.333 55.333;" xml
  c-1.471,0-2.667-1.196-2.667-2.667s1.196-2.667,2.667-2.667s2.667,1.196,2.667,2.667S43.639,30.834,42.167,30.834z M42.167,38.334
  c-3.125,0-5.667,2.542-5.667,5.667s2.542,5.666,5.667,5.666s5.667-2.542,5.667-5.667S45.292,38.334,42.167,38.334z M42.167,46.667
  c-1.471,0-2.667-1.196-2.667-2.667s1.196-2.667,2.667-2.667s2.667,1.196,2.667,2.667S43.639,46.667,42.167,46.667z"/>
-</g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>`
+</g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>`;
 
 const rollIcon = `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 viewBox="0 0 512.004 512.004" style="enable-background:new 0 0 512.004 512.004;" xml:space="preserve">
@@ -25,21 +25,21 @@ viewBox="0 0 512.004 512.004" style="enable-background:new 0 0 512.004 512.004;"
 <path fill="#050038" d="M405.333,85.335c-11.776,0-21.333,9.557-21.333,21.333s9.557,21.333,21.333,21.333s21.333-9.557,21.333-21.333S417.109,85.335,405.333,85.335z"/>
 <path fill="#050038" d="M399.032,265.921l-54.092,54.073h-28.602c2.373-6.678,3.666-13.859,3.666-21.333c0-35.249-28.751-64-64-64H152.796L128,226.403v-34.401c0-11.782-9.551-21.333-21.333-21.333H21.333C9.551,170.669,0,180.22,0,192.002v298.667c0,11.782,9.551,21.333,21.333,21.333h85.333c11.782,0,21.333-9.551,21.333-21.333v-21.342h127.066c60.397,0,119.538-17.22,170.512-49.648l3.637-2.917l60.331-60.352c24.927-24.895,24.927-65.568-0.003-90.498C464.612,240.981,423.94,240.981,399.032,265.921z M85.333,469.336H42.667v-256h42.667V469.336z M459.382,326.232l-58.654,58.674c-43.686,27.281-94.146,41.754-145.663,41.754H128.004V271.375l14.593,4.86c2.173,0.724,4.449,1.093,6.74,1.093h106.667c11.685,0,21.333,9.649,21.333,21.333c0,11.685-9.649,21.333-21.333,21.333c-28.444,0-28.444,42.667,0,42.667h97.771c5.657,0,11.082-2.247,15.082-6.246l60.352-60.331c8.257-8.267,21.899-8.267,30.163-0.003S467.637,317.988,459.382,326.232z"/>
 </g></g></g>
-</svg>`
+</svg>`;
 
-const num_to_str_dict = {
-  1: 'one',
-  2: 'two',
-  3: 'three',
-  4: 'four',
-  5: 'five',
-  6: 'six'
-}
+const edges = [
+  `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" viewBox="0 0 640 640" width="640" height="640"><defs><path d="M551.45 76.61C558.04 76.61 563.39 81.96 563.39 88.55C563.39 183.52 563.39 456.48 563.39 551.45C563.39 558.04 558.04 563.39 551.45 563.39C456.48 563.39 183.52 563.39 88.55 563.39C81.96 563.39 76.61 558.04 76.61 551.45C76.61 456.48 76.61 183.52 76.61 88.55C76.61 81.96 81.96 76.61 88.55 76.61C183.52 76.61 456.48 76.61 551.45 76.61Z" id="g6H4LGxhB"></path><mask id="maska3pgC8soar" x="-27.39" y="-27.39" width="694.78" height="694.78" maskUnits="userSpaceOnUse"><rect x="-27.39" y="-27.39" width="694.78" height="694.78" fill="white"></rect><use xlink:href="#g6H4LGxhB" opacity="1" fill="black"></use></mask><path d="M385.51 308.29C385.51 345.05 355.66 374.9 318.9 374.9C282.14 374.9 252.29 345.05 252.29 308.29C252.29 271.53 282.14 241.68 318.9 241.68C355.66 241.68 385.51 271.53 385.51 308.29Z" id="aDHxKg5Fz"></path></defs><g><g><g><g mask="url(#maska3pgC8soar)"><use xlink:href="#g6H4LGxhB" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="104" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#aDHxKg5Fz" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="52" stroke-opacity="1"></use></g></g></g></g></svg>`,
+  '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" viewBox="0 0 640 640" width="640" height="640"><defs><path d="M551.45 76.61C558.04 76.61 563.39 81.96 563.39 88.55C563.39 183.52 563.39 456.48 563.39 551.45C563.39 558.04 558.04 563.39 551.45 563.39C456.48 563.39 183.52 563.39 88.55 563.39C81.96 563.39 76.61 558.04 76.61 551.45C76.61 456.48 76.61 183.52 76.61 88.55C76.61 81.96 81.96 76.61 88.55 76.61C183.52 76.61 456.48 76.61 551.45 76.61Z" id="aS43vyYgs"></path><mask id="maskd1wJMdMhmg" x="-27.39" y="-27.39" width="694.78" height="694.78" maskUnits="userSpaceOnUse"><rect x="-27.39" y="-27.39" width="694.78" height="694.78" fill="white"></rect><use xlink:href="#aS43vyYgs" opacity="1" fill="black"></use></mask><path d="M225.82 481C225.82 510.49 201.88 534.43 172.39 534.43C142.9 534.43 118.96 510.49 118.96 481C118.96 451.51 142.9 427.57 172.39 427.57C201.88 427.57 225.82 451.51 225.82 481Z" id="a4Nyg19AlT"></path><path d="M516.44 160.23C516.44 189.72 492.5 213.66 463.01 213.66C433.52 213.66 409.58 189.72 409.58 160.23C409.58 130.74 433.52 106.8 463.01 106.8C492.5 106.8 516.44 130.74 516.44 160.23Z" id="b1oXYbafFQ"></path></defs><g><g><g><g mask="url(#maskd1wJMdMhmg)"><use xlink:href="#aS43vyYgs" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="104" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#a4Nyg19AlT" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#b1oXYbafFQ" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g></g></g></svg>',
+  '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" viewBox="0 0 640 640" width="640" height="640"><defs><path d="M551.45 76.61C558.04 76.61 563.39 81.96 563.39 88.55C563.39 183.52 563.39 456.48 563.39 551.45C563.39 558.04 558.04 563.39 551.45 563.39C456.48 563.39 183.52 563.39 88.55 563.39C81.96 563.39 76.61 558.04 76.61 551.45C76.61 456.48 76.61 183.52 76.61 88.55C76.61 81.96 81.96 76.61 88.55 76.61C183.52 76.61 456.48 76.61 551.45 76.61Z" id="aS43vyYgs"></path><mask id="maskd1wJMdMhmg" x="-27.39" y="-27.39" width="694.78" height="694.78" maskUnits="userSpaceOnUse"><rect x="-27.39" y="-27.39" width="694.78" height="694.78" fill="white"></rect><use xlink:href="#aS43vyYgs" opacity="1" fill="black"></use></mask><path d="M225.82 481C225.82 510.49 201.88 534.43 172.39 534.43C142.9 534.43 118.96 510.49 118.96 481C118.96 451.51 142.9 427.57 172.39 427.57C201.88 427.57 225.82 451.51 225.82 481Z" id="a4Nyg19AlT"></path><path d="M516.44 160.23C516.44 189.72 492.5 213.66 463.01 213.66C433.52 213.66 409.58 189.72 409.58 160.23C409.58 130.74 433.52 106.8 463.01 106.8C492.5 106.8 516.44 130.74 516.44 160.23Z" id="b1oXYbafFQ"></path><path d="M373.43 320C373.43 349.49 349.49 373.43 320 373.43C290.51 373.43 266.57 349.49 266.57 320C266.57 290.51 290.51 266.57 320 266.57C349.49 266.57 373.43 290.51 373.43 320Z" id="cwzA75oP9"></path></defs><g><g><g><g mask="url(#maskd1wJMdMhmg)"><use xlink:href="#aS43vyYgs" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="104" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#a4Nyg19AlT" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#b1oXYbafFQ" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#cwzA75oP9" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g></g></g></svg>',
+  '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" viewBox="0 0 640 640" width="640" height="640"><defs><path d="M551.45 76.61C558.04 76.61 563.39 81.96 563.39 88.55C563.39 183.52 563.39 456.48 563.39 551.45C563.39 558.04 558.04 563.39 551.45 563.39C456.48 563.39 183.52 563.39 88.55 563.39C81.96 563.39 76.61 558.04 76.61 551.45C76.61 456.48 76.61 183.52 76.61 88.55C76.61 81.96 81.96 76.61 88.55 76.61C183.52 76.61 456.48 76.61 551.45 76.61Z" id="aS43vyYgs"></path><mask id="maskd1wJMdMhmg" x="-27.39" y="-27.39" width="694.78" height="694.78" maskUnits="userSpaceOnUse"><rect x="-27.39" y="-27.39" width="694.78" height="694.78" fill="white"></rect><use xlink:href="#aS43vyYgs" opacity="1" fill="black"></use></mask><path d="M225.82 160.23C225.82 189.72 201.88 213.66 172.39 213.66C142.9 213.66 118.96 189.72 118.96 160.23C118.96 130.74 142.9 106.8 172.39 106.8C201.88 106.8 225.82 130.74 225.82 160.23Z" id="bjtFzNaEU"></path><path d="M225.82 481C225.82 510.49 201.88 534.43 172.39 534.43C142.9 534.43 118.96 510.49 118.96 481C118.96 451.51 142.9 427.57 172.39 427.57C201.88 427.57 225.82 451.51 225.82 481Z" id="a4Nyg19AlT"></path><path d="M516.44 160.23C516.44 189.72 492.5 213.66 463.01 213.66C433.52 213.66 409.58 189.72 409.58 160.23C409.58 130.74 433.52 106.8 463.01 106.8C492.5 106.8 516.44 130.74 516.44 160.23Z" id="b1oXYbafFQ"></path><path d="M516.44 481C516.44 510.49 492.5 534.43 463.01 534.43C433.52 534.43 409.58 510.49 409.58 481C409.58 451.51 433.52 427.57 463.01 427.57C492.5 427.57 516.44 451.51 516.44 481Z" id="cwSIaHto2"></path></defs><g><g><g><g mask="url(#maskd1wJMdMhmg)"><use xlink:href="#aS43vyYgs" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="104" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#bjtFzNaEU" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#a4Nyg19AlT" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#b1oXYbafFQ" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#cwSIaHto2" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g></g></g></svg>',
+  '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" viewBox="0 0 640 640" width="640" height="640"><defs><path d="M551.45 76.61C558.04 76.61 563.39 81.96 563.39 88.55C563.39 183.52 563.39 456.48 563.39 551.45C563.39 558.04 558.04 563.39 551.45 563.39C456.48 563.39 183.52 563.39 88.55 563.39C81.96 563.39 76.61 558.04 76.61 551.45C76.61 456.48 76.61 183.52 76.61 88.55C76.61 81.96 81.96 76.61 88.55 76.61C183.52 76.61 456.48 76.61 551.45 76.61Z" id="aS43vyYgs"></path><mask id="maskd1wJMdMhmg" x="-27.39" y="-27.39" width="694.78" height="694.78" maskUnits="userSpaceOnUse"><rect x="-27.39" y="-27.39" width="694.78" height="694.78" fill="white"></rect><use xlink:href="#aS43vyYgs" opacity="1" fill="black"></use></mask><path d="M225.82 160.23C225.82 189.72 201.88 213.66 172.39 213.66C142.9 213.66 118.96 189.72 118.96 160.23C118.96 130.74 142.9 106.8 172.39 106.8C201.88 106.8 225.82 130.74 225.82 160.23Z" id="bjtFzNaEU"></path><path d="M225.82 481C225.82 510.49 201.88 534.43 172.39 534.43C142.9 534.43 118.96 510.49 118.96 481C118.96 451.51 142.9 427.57 172.39 427.57C201.88 427.57 225.82 451.51 225.82 481Z" id="a4Nyg19AlT"></path><path d="M516.44 160.23C516.44 189.72 492.5 213.66 463.01 213.66C433.52 213.66 409.58 189.72 409.58 160.23C409.58 130.74 433.52 106.8 463.01 106.8C492.5 106.8 516.44 130.74 516.44 160.23Z" id="b1oXYbafFQ"></path><path d="M373.43 320C373.43 349.49 349.49 373.43 320 373.43C290.51 373.43 266.57 349.49 266.57 320C266.57 290.51 290.51 266.57 320 266.57C349.49 266.57 373.43 290.51 373.43 320Z" id="b9h0LNu4i"></path><path d="M516.44 481C516.44 510.49 492.5 534.43 463.01 534.43C433.52 534.43 409.58 510.49 409.58 481C409.58 451.51 433.52 427.57 463.01 427.57C492.5 427.57 516.44 451.51 516.44 481Z" id="cwSIaHto2"></path></defs><g><g><g><g mask="url(#maskd1wJMdMhmg)"><use xlink:href="#aS43vyYgs" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="104" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#bjtFzNaEU" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#a4Nyg19AlT" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#b1oXYbafFQ" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#b9h0LNu4i" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#cwSIaHto2" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g></g></g></svg>',
+  '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" viewBox="0 0 640 640" width="640" height="640"><defs><path d="M551.45 76.61C558.04 76.61 563.39 81.96 563.39 88.55C563.39 183.52 563.39 456.48 563.39 551.45C563.39 558.04 558.04 563.39 551.45 563.39C456.48 563.39 183.52 563.39 88.55 563.39C81.96 563.39 76.61 558.04 76.61 551.45C76.61 456.48 76.61 183.52 76.61 88.55C76.61 81.96 81.96 76.61 88.55 76.61C183.52 76.61 456.48 76.61 551.45 76.61Z" id="aS43vyYgs"></path><mask id="maskd1wJMdMhmg" x="-27.39" y="-27.39" width="694.78" height="694.78" maskUnits="userSpaceOnUse"><rect x="-27.39" y="-27.39" width="694.78" height="694.78" fill="white"></rect><use xlink:href="#aS43vyYgs" opacity="1" fill="black"></use></mask><path d="M225.82 160.23C225.82 189.72 201.88 213.66 172.39 213.66C142.9 213.66 118.96 189.72 118.96 160.23C118.96 130.74 142.9 106.8 172.39 106.8C201.88 106.8 225.82 130.74 225.82 160.23Z" id="bjtFzNaEU"></path><path d="M225.82 320C225.82 349.49 201.88 373.43 172.39 373.43C142.9 373.43 118.96 349.49 118.96 320C118.96 290.51 142.9 266.57 172.39 266.57C201.88 266.57 225.82 290.51 225.82 320Z" id="b7yRXkbGP"></path><path d="M225.82 481C225.82 510.49 201.88 534.43 172.39 534.43C142.9 534.43 118.96 510.49 118.96 481C118.96 451.51 142.9 427.57 172.39 427.57C201.88 427.57 225.82 451.51 225.82 481Z" id="a4Nyg19AlT"></path><path d="M516.44 160.23C516.44 189.72 492.5 213.66 463.01 213.66C433.52 213.66 409.58 189.72 409.58 160.23C409.58 130.74 433.52 106.8 463.01 106.8C492.5 106.8 516.44 130.74 516.44 160.23Z" id="b1oXYbafFQ"></path><path d="M516.44 320C516.44 349.49 492.5 373.43 463.01 373.43C433.52 373.43 409.58 349.49 409.58 320C409.58 290.51 433.52 266.57 463.01 266.57C492.5 266.57 516.44 290.51 516.44 320Z" id="b361ksO6iL"></path><path d="M516.44 481C516.44 510.49 492.5 534.43 463.01 534.43C433.52 534.43 409.58 510.49 409.58 481C409.58 451.51 433.52 427.57 463.01 427.57C492.5 427.57 516.44 451.51 516.44 481Z" id="cwSIaHto2"></path></defs><g><g><g><g mask="url(#maskd1wJMdMhmg)"><use xlink:href="#aS43vyYgs" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="104" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#bjtFzNaEU" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#b7yRXkbGP" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#a4Nyg19AlT" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#b1oXYbafFQ" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#b361ksO6iL" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#cwSIaHto2" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g></g></g></svg>'
+];
 
 
 miro.onReady(async () => {
 	miro.currentUser.getId().then(user_id =>
-		gtag('set', {'user_id': user_id}))
+		gtag('set', {'user_id': user_id}));
 	miro.initialize({
 		extensionPoints: {
       toolbar: {
@@ -52,27 +52,27 @@ miro.onReady(async () => {
       },
 			getWidgetMenuItems: (widgets) => {
         clientId = miro.getClientId()
-				if (widgets.some((widget) => widget.type.toLowerCase() != 'image' && (!(clientId in widget.metadata) || !(isDice in widget.metadata.clientId) || widget.metadata.clientId.isDice == false)) == false){
+				if (widgets.every((widget) => widget.type.toLowerCase() == 'emoji' && widget.metadata[clientId] && widget.metadata[clientId].isDice)){
 					return Promise.resolve([{
 						tooltip: 'Roll',
 						svgIcon: rollIcon,
 						onClick: rollDice
           }])
 				}
-				return Promise.resolve([{}])
+				return Promise.resolve([{}]);
 			}
 		}
-	})
+	});
   Object.defineProperty(window, 'team_id', {
       value: (await miro.account.get())['id'],
       configurable: false,
       writable: false
-  })
+  });
   Object.defineProperty(window, 'user_id', {
       value: await miro.currentUser.getId(),
       configurable: false,
       writable: false
-  })
+  });
   $.ajax({
       url: '/startSession',
       method: 'GET',
@@ -83,25 +83,25 @@ miro.onReady(async () => {
           user_id: user_id,
           team_id: team_id
       }
-  })
-})
+  });
+});
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 async function rollDice() {
   let new_widgets = [];
   (await miro.board.selection.get()).forEach((widget) => {
     new_widgets.push({
       id: widget['id'],
-      url: widget['url'].substr(0, widget['url'].lastIndexOf('/')) + `/${num_to_str_dict[getRandomInt(1,6)]}_dot.svg`
-    })
-  })
-  miro.board.widgets.update(new_widgets)
-}
+      code: edges[getRandomInt(0,5)]
+    });
+  });
+  miro.board.widgets.update(new_widgets);
+};
 
 window.addEventListener("beforeunload", async function (e) {
   await $.ajax({
@@ -114,5 +114,5 @@ window.addEventListener("beforeunload", async function (e) {
       user_id: user_id,
       team_id: team_id
     }
-  })
+  });
 });
