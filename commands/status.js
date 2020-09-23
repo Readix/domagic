f6fe763f153ad00dc6710425eb7e3e35e0cee3bb
Kirill <readix108@gmail.com>
2020-09-23 23:40:51 +0000

    add the status function in npm run

---
+++ commands/status.js
@@ -0,0 +1,99 @@
+const errh = require('./_errhandler')
+const db = require('../src/db')
+const config = require('../src/config.json')
+const {getPluginsList} = require('./_functions')
+const fs = require('fs')
+
+
+if (require.main !== module) {
+    module.exports = {
+        name: 'status'
+    }
+    return
+}
+
+/* Main functions */
+
+let dbConn = async () => {
+    return db.init()
+        .then(initMsg => true)
+        .catch(err => false)
+}
+
+let dbSync = async (pluginName) => {
+    return (await getPluginsList())
+        .map(props => props.name)
+        .indexOf(pluginName) > -1
+}
+
+let frontExists = async (pluginName) => {
+    let srcDir = __dirname.split('/').slice(0, -1).join('/')
+    let filenames = fs.readdirSync(srcDir + '/static/')
+    if (filenames.some(name => name == pluginName)) {
+        return true 
+    }
+    return false
+}
+
+let statuses = {
+    commons: {
+        'database connection': dbConn
+    },
+    special: {
+        'database sync': dbSync, 
+        'frontend': frontExists
+    }
+}
+
+let printWithTabs = (msg) => 
+    console.log('\t'.repeat(printWithTabs.tabsCount) + msg)
+printWithTabs.tabsCount = 0
+
+const colon = (key, val) => key + ': ' + val
+
+let okString = '\x1b[32mok\x1b[0m'
+let failString = '\x1b[41mfail\x1b[0m'
+
+let commonsPromises = []
+Object.keys(statuses.commons)
+    .forEach(testname => {
+        let promise = statuses.commons[testname]()
+            .then(status => {
+                return () => printWithTabs(
+                    testname + ': ' + 
+                    (status ? okString : failString)
+                )
+            })
+        commonsPromises.push(promise)
+    })
+
+let pluginsPromises = []
+Promise.all(commonsPromises)
+    .then(prints => {
+        prints.forEach(print => print())
+        config.plugins.forEach(pluginName => {
+            pluginsPromises.push(() => {
+                printWithTabs(pluginName + ':')
+            })
+            Object.keys(statuses.special)
+                .forEach(testname => {
+                    let promise = statuses.special[testname](pluginName)
+                        .then(status => {
+                            return () => {
+                                str = testname + ': ' + 
+                                    (status ? okString : failString)
+                                printWithTabs.tabsCount += 1
+                                printWithTabs(str)
+                                printWithTabs.tabsCount -= 1
+                            }
+                        })
+                    pluginsPromises.push(promise)
+                })
+        })
+        Promise.all(pluginsPromises)
+        .then(prints => {
+            prints.forEach(print => print())
+            process.exit()
+        })  
+    })
+
