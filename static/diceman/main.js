b64cca9cdc40a2d818b7ac90ae9451213d3c3cb2
Kirill <readix108@gmail.com>
2020-10-29 18:53:05 +0000

    update autentication

--- static/diceman/main.js
+++ static/diceman/main.js
@@ -35,9 +35,37 @@ const edges = [
   '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" viewBox="0 0 640 640" width="640" height="640"><defs><path d="M551.45 76.61C558.04 76.61 563.39 81.96 563.39 88.55C563.39 183.52 563.39 456.48 563.39 551.45C563.39 558.04 558.04 563.39 551.45 563.39C456.48 563.39 183.52 563.39 88.55 563.39C81.96 563.39 76.61 558.04 76.61 551.45C76.61 456.48 76.61 183.52 76.61 88.55C76.61 81.96 81.96 76.61 88.55 76.61C183.52 76.61 456.48 76.61 551.45 76.61Z" id="aS43vyYgs"></path><mask id="maskd1wJMdMhmg" x="-27.39" y="-27.39" width="694.78" height="694.78" maskUnits="userSpaceOnUse"><rect x="-27.39" y="-27.39" width="694.78" height="694.78" fill="white"></rect><use xlink:href="#aS43vyYgs" opacity="1" fill="black"></use></mask><path d="M225.82 160.23C225.82 189.72 201.88 213.66 172.39 213.66C142.9 213.66 118.96 189.72 118.96 160.23C118.96 130.74 142.9 106.8 172.39 106.8C201.88 106.8 225.82 130.74 225.82 160.23Z" id="bjtFzNaEU"></path><path d="M225.82 481C225.82 510.49 201.88 534.43 172.39 534.43C142.9 534.43 118.96 510.49 118.96 481C118.96 451.51 142.9 427.57 172.39 427.57C201.88 427.57 225.82 451.51 225.82 481Z" id="a4Nyg19AlT"></path><path d="M516.44 160.23C516.44 189.72 492.5 213.66 463.01 213.66C433.52 213.66 409.58 189.72 409.58 160.23C409.58 130.74 433.52 106.8 463.01 106.8C492.5 106.8 516.44 130.74 516.44 160.23Z" id="b1oXYbafFQ"></path><path d="M373.43 320C373.43 349.49 349.49 373.43 320 373.43C290.51 373.43 266.57 349.49 266.57 320C266.57 290.51 290.51 266.57 320 266.57C349.49 266.57 373.43 290.51 373.43 320Z" id="b9h0LNu4i"></path><path d="M516.44 481C516.44 510.49 492.5 534.43 463.01 534.43C433.52 534.43 409.58 510.49 409.58 481C409.58 451.51 433.52 427.57 463.01 427.57C492.5 427.57 516.44 451.51 516.44 481Z" id="cwSIaHto2"></path></defs><g><g><g><g mask="url(#maskd1wJMdMhmg)"><use xlink:href="#aS43vyYgs" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="104" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#bjtFzNaEU" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#a4Nyg19AlT" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#b1oXYbafFQ" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#b9h0LNu4i" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#cwSIaHto2" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g></g></g></svg>',
   '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" viewBox="0 0 640 640" width="640" height="640"><defs><path d="M551.45 76.61C558.04 76.61 563.39 81.96 563.39 88.55C563.39 183.52 563.39 456.48 563.39 551.45C563.39 558.04 558.04 563.39 551.45 563.39C456.48 563.39 183.52 563.39 88.55 563.39C81.96 563.39 76.61 558.04 76.61 551.45C76.61 456.48 76.61 183.52 76.61 88.55C76.61 81.96 81.96 76.61 88.55 76.61C183.52 76.61 456.48 76.61 551.45 76.61Z" id="aS43vyYgs"></path><mask id="maskd1wJMdMhmg" x="-27.39" y="-27.39" width="694.78" height="694.78" maskUnits="userSpaceOnUse"><rect x="-27.39" y="-27.39" width="694.78" height="694.78" fill="white"></rect><use xlink:href="#aS43vyYgs" opacity="1" fill="black"></use></mask><path d="M225.82 160.23C225.82 189.72 201.88 213.66 172.39 213.66C142.9 213.66 118.96 189.72 118.96 160.23C118.96 130.74 142.9 106.8 172.39 106.8C201.88 106.8 225.82 130.74 225.82 160.23Z" id="bjtFzNaEU"></path><path d="M225.82 320C225.82 349.49 201.88 373.43 172.39 373.43C142.9 373.43 118.96 349.49 118.96 320C118.96 290.51 142.9 266.57 172.39 266.57C201.88 266.57 225.82 290.51 225.82 320Z" id="b7yRXkbGP"></path><path d="M225.82 481C225.82 510.49 201.88 534.43 172.39 534.43C142.9 534.43 118.96 510.49 118.96 481C118.96 451.51 142.9 427.57 172.39 427.57C201.88 427.57 225.82 451.51 225.82 481Z" id="a4Nyg19AlT"></path><path d="M516.44 160.23C516.44 189.72 492.5 213.66 463.01 213.66C433.52 213.66 409.58 189.72 409.58 160.23C409.58 130.74 433.52 106.8 463.01 106.8C492.5 106.8 516.44 130.74 516.44 160.23Z" id="b1oXYbafFQ"></path><path d="M516.44 320C516.44 349.49 492.5 373.43 463.01 373.43C433.52 373.43 409.58 349.49 409.58 320C409.58 290.51 433.52 266.57 463.01 266.57C492.5 266.57 516.44 290.51 516.44 320Z" id="b361ksO6iL"></path><path d="M516.44 481C516.44 510.49 492.5 534.43 463.01 534.43C433.52 534.43 409.58 510.49 409.58 481C409.58 451.51 433.52 427.57 463.01 427.57C492.5 427.57 516.44 451.51 516.44 481Z" id="cwSIaHto2"></path></defs><g><g><g><g mask="url(#maskd1wJMdMhmg)"><use xlink:href="#aS43vyYgs" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="104" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#bjtFzNaEU" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#b7yRXkbGP" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#a4Nyg19AlT" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#b1oXYbafFQ" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#b361ksO6iL" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g><g><g><use xlink:href="#cwSIaHto2" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="44" stroke-opacity="1"></use></g></g></g></g></svg>'
 ];
+let tooltipLabel = 'Roll'
+let isAuth = async () => {
+  return await $.ajax({
+    url: '/plugin/diceman/auth',
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    data: {
+      access_token: await miro.getToken()
+    }
+  }).then(res => {
+  	switch(parseInt(res.code / 100)){
+  		case 2: 
+  		    miro.showErrorNotification(res.message);
+  		    return false;
+  		case 1:     
+  		    miro.showNotification(res.message);
+  		    return true;
+  		default: return true;
+  	}
+  }, () => {
+    miro.showErrorNotification('Server error, please, try again later');
+    return false;
+  })
+}
+let authResponse = {code: 201, message: 'Something goes wrong, try again later'}
 
 gah.wait(5, 20 * 60, () => gah.event('DiceMan', 'use_often', '', 500))
 miro.onReady(async () => {
+  isAuth().then(res => {authResponse = res})
 	miro.currentUser.getId().then(user_id =>
     gah.setUser(user_id));
   gah.event('DiceMan', 'available', 'true', 10)
@@ -55,7 +83,7 @@ miro.onReady(async () => {
         clientId = miro.getClientId()
 				if (widgets.every((widget) => widget.type.toLowerCase() == 'emoji' && widget.metadata[clientId] && widget.metadata[clientId].isDice)){
 					return Promise.resolve([{
-						tooltip: 'Roll',
+						tooltip: tooltipLabel,
 						svgIcon: rollIcon,
 						onClick: rollDice
           }])
@@ -99,31 +127,17 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-async function rollDice() {  
-  auth = await $.ajax({
-    url: '/plugin/hideman/auth',
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    data: {
-      access_token: await miro.getToken()
-    }
-  }).then(res => {
-    switch(parseInt(res.code / 100)){
-      case 2: 
-          miro.showErrorNotification(res.message);
-          return false;
-      case 1:     
-          miro.showNotification(res.message);
-          return true;
-      default: return true;
-    }
-  }, () => {
-    miro.showErrorNotification('Server error, please, try again later');
-    return false;
-  })
-  if (!auth) return;
+async function rollDice() {
+  switch(parseInt(authResponse.code / 100)){
+    case 5:
+      miro.showErrorNotification(authResponse.message);
+      return;
+    case 2: 
+      miro.showErrorNotification(authResponse.message);
+      return;
+    case 1:
+      miro.showNotification(authResponse.message);
+  }
 
   let new_widgets = [];
   let gahSizes = [];
--- static/hideman/main.js
+++ static/hideman/main.js
@@ -1,10 +1,27 @@
 let icon = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 36 45" style="enable-background:new 0 0 36 36;" xml:space="preserve"><g><path d="M34.3,31.3c-0.8,0.9-1.9,1.6-3.2,1.6H5c-1.5,0-2.8-0.8-3.5-2c-0.6-0.9-0.6-2.1-0.2-3.1l5.4-13c0.7-2.3,2.4-4.2,4.7-4.2   L4.1,28.1c-0.4,1,0,1.7,1.7,1.7h24.5c1.7,0,2.1-0.9,1.7-1.7l-7.9-17.7c0,0,0-0.1,0-0.1v0c-2.7-5.4-4.2-3.4-5,0c0,0,0,0.1,0,0.1   c-0.2,1-0.4,2-0.5,3.1c-0.3,2.9-0.4,5.7-0.4,5.7h3.4c0.5,0,0.4,0.6,0.2,0.9l-4.4,6.3l-1.2,1.7l-0.5,0.8c-0.2,0.2-0.6,0.2-0.8,0   l-0.5-0.8L13,26.4L8.6,20c-0.1-0.1-0.2-0.3-0.2-0.4c0-0.2,0.1-0.4,0.3-0.4h2.8c0.4-2.1,0.8-3.9,1.2-5.5c0.3-1.1,0.6-2.2,0.9-3.1   c0-0.1,0-0.2,0.1-0.2c4.8-14.4,11.4-3.6,14,1.7c0.7,1.3,1.1,2.3,1.1,2.5l0,0l5.9,13C35.3,28.8,35.2,30.2,34.3,31.3z"/></g><text x="0" y="51" fill="#000000" font-size="5px" font-weight="bold" font-family="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif">Created by Nathan Smith</text><text x="0" y="56" fill="#000000" font-size="5px" font-weight="bold" font-family="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif">from the Noun Project</text></svg>`;
 let tooltipLabel = 'Flip'
 
+let isAuth = async () => {
+  return await $.ajax({
+    url: '/plugin/hideman/auth',
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    data: {
+      access_token: await miro.getToken()
+    }
+  }).then(res => res, () => {
+    return {code: 500, message: 'Server error, please, try again later'};
+  })
+}
+
+let authResponse = {code: 201, message: 'Something goes wrong, try again later'}
+
 gah.wait(5, 20 * 60, () => gah.event('HideMan', 'use_often', '', 500))
 miro.onReady(async () => {
-	miro.currentUser.getId().then(user_id =>
-    gah.setUser(user_id));
+  isAuth().then(res => {authResponse = res})
+	miro.currentUser.getId().then(user_id => gah.setUser(user_id));
   gah.event('HideMan', 'available', 'true', 10)
   let allowedStandaloneWidgetTypes = ['shape', 'sticker'];
   let allowedGroupedWidgetTypes = [...allowedStandaloneWidgetTypes, 'image', 'text'];
@@ -23,9 +40,9 @@ miro.onReady(async () => {
           let groupedWidgets = widgets.filter((widget) => !standaloneWidgets.includes(widget));
 
           if (standaloneWidgets.some((widget) => !standaloneWidgetTypeIsCorrect(widget.type.toLowerCase())))
-            return Promise.resolve([{}]);
+            return Promise.resolve([]);
           if (groupedWidgets.some((widget) => !groupedWidgetTypeIsCorrect(widget.type.toLowerCase())))
-            return Promise.resolve([{}]);
+            return Promise.resolve([]);
 
           let groupIds = groupedWidgets.map((widget) => widget.groupId).reduce((acc, curr) => acc.includes(curr) ? acc : [...acc, curr], []);
           
@@ -85,45 +102,29 @@ miro.onReady(async () => {
       writable: false
   });
   $.ajax({
-      url: '/startSession',
-      method: 'GET',
+      url: '/user/startSession',
+      method: 'POST',
       headers: {
           'Content-Type': 'application/json'
       },
-      data: {
-          user_id: user_id,
-          team_id: team_id
-      }
+      data: JSON.stringify({
+        access_token: await miro.getToken()
+      })
   });
 })
 
 async function flipWidgets(widgets){
-  auth = await $.ajax({
-    url: '/plugin/hideman/auth',
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    data: {
-      access_token: await miro.getToken()
-    }
-  }).then(res => {
-  	switch(parseInt(res.code / 100)){
-  		case 2: 
-  		    miro.showErrorNotification(res.message);
-  		    return false;
-  		case 1:     
-  		    miro.showNotification(res.message);
-  		    return true;
-  		default: return true;
-  	}
-  }, () => {
-    miro.showErrorNotification('Server error, please, try again later');
-    return false;
-  })
-  if (!auth) return;
+  switch(parseInt(authResponse.code / 100)){
+    case 5:
+      miro.showErrorNotification(authResponse.message);
+      return;
+    case 2: 
+      miro.showErrorNotification(authResponse.message);
+      return;
+    case 1:
+      miro.showNotification(authResponse.message);
+  }
 
-  console.log(res)
   gah.pin()
   gah.event('HideMan', 'use', widgets.length, 50)
   let clientId = await miro.getClientId();
@@ -196,14 +197,13 @@ async function flipWidgets(widgets){
 
 window.addEventListener("beforeunload", async function (e) {
   await $.ajax({
-    url: '/endSession',
-    method: 'GET',
+    url: '/user/endSession',
+    method: 'POST',
     headers: {
         'Content-Type': 'application/json'
     },
-    data: {
-      user_id: user_id,
-      team_id: team_id
-    }
+    data: JSON.stringify({
+      access_token: await miro.getToken()
+    })
   })
 });
